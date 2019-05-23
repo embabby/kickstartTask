65282f768375091672fdec5d09a5cbc1549f6426
embaby <mohamed.nabil@at-portal.info>
2019-05-23 11:30:51 +0200

    update index

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,16 +4,24 @@
 
 <style>
   #map{
-    width: 550px;
     height: 450px;
   }
 </style>
-
-
- <div id="map" style="margin-top: 10px;"> 
+<div style="overflow: hidden;">
+	
+<div class="row">
+	
+ <div id="map" class="col-md-6" style="margin-top: 10px; display: inline-block;"> 
 
 </div>
 
+	<div class="col-md-2" style="margin-top: 10px; display: inline-block;">
+		<p><span> Neutral:</span> <img src="{{asset('images/Neutral.png')}}"></p>
+		<p><span> Positive:</span> <img src="{{asset('images/Positive.png')}}"></p>
+		<p><span> Negative:</span> <img src="{{asset('images/Negative.png')}}"></p>
+	</div>
+</div>
+</div>
 
 @stop
 
