a21ec71d5f6bb0f97c83e8550ab894572627228c
embabby <46160902+embabby@users.noreply.github.com>
2020-03-28 02:17:56 +0200

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,10 +10,11 @@
 
 </div>
 
-	<div class="col-md-2" style="margin-top: 10px; display: inline-block;">
+	<div class="col-md-4" style="margin-top: 10px; display: inline-block;">
 		<p><span> Neutral:</span> <img src="{{asset('images/Neutral.png')}}"></p>
 		<p><span> Positive:</span> <img src="{{asset('images/Positive.png')}}"></p>
 		<p><span> Negative:</span> <img src="{{asset('images/Negative.png')}}"></p>
+		<small>*hover on each flag to read the message and the date*</small>
 	</div>
 </div>
 </div>
